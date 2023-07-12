7298acb7bfdda57ec78177f2edceed123aee5de4
Ryan Horne <ryan.matthew.horne@gmail.com>
2023-07-12 14:20:08 -0700

    Updated index to create a new feature when it is clicked on
    Now the new feature and information is derived from the datatable directly, which eliminates the long index search in leaflet

--- pliny_main.js
+++ pliny_main.js
@@ -1,35 +1,70 @@
 $(document).ready(function() {
     // Initialize Leaflet map
-    var map = L.map('map').setView([0, 0], 2);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    var map = L.map('map').setView([51.505, -0.09], 13);
 
-    // Initialize DataTables
-    var table = $('#indexTable').DataTable();
+    // Add the base tile layer to the map
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+    }).addTo(map);
 
-    // Toggle button functionality
-    $('#toggleBtn').on('click', function() {
-        $('#indexContainer').toggle();
-    });
+    // Initialize DataTable
+    var datatable = $('#datatable').DataTable();
 
-    // Read GeoJSON file and add features to map and index
+    // Variable to store the temporary marker
+    var tempMarker = null;
+
+    // Read the GeoJSON file
     $.getJSON('Pliny_Points.geojson', function(data) {
-        L.geoJSON(data, {
-            onEachFeature: function(feature, layer) {
-                // Add feature to map
-                layer.addTo(map);
+        // Loop through each feature
+        data.features.forEach(function(feature) {
+            // Get the coordinates of the feature
+            var coordinates = feature.geometry.coordinates;
 
-                // Add feature to index
-                table.row.add([
-                    feature.properties.Dir_entry1,
-                    feature.geometry.coordinates.toString()
-                ]).draw();
+            // Create a marker at the coordinates
+            var marker = L.marker([coordinates[1], coordinates[0]]);
+            
+            // Add a popup to the marker with data from DataTables
+            marker.bindPopup(feature.properties.Dir_entry1);
 
-                // Zoom and open popup on table row click
-                layer.on('click', function() {
-                    map.setView(layer.getBounds().getCenter());
-                    layer.openPopup();
-                });
+            // Add the marker to the map
+            marker.addTo(map);
+
+            // Add a row to the DataTable
+            datatable.row.add([feature.properties.Dir_entry1]).draw();
+        });
+
+        // Handle row click event
+        $('#datatable tbody').on('click', 'tr', function() {
+            // Clear the temporary marker if exists
+            if (tempMarker) {
+                map.removeLayer(tempMarker);
             }
+
+            // Get the index of the clicked row
+            var index = datatable.row(this).index();
+
+            // Get the corresponding feature from the GeoJSON
+            var feature = data.features[index];
+
+            // Get the coordinates from DataTables
+            var coordinates = feature.geometry.coordinates;
+
+            // Create a temporary marker with an orange circle
+            tempMarker = L.circleMarker([coordinates[1], coordinates[0]], {
+                color: 'orange',
+                radius: 8,
+                fillOpacity: 1
+            }).addTo(map);
+            
+            // Add a popup to the temporary marker with data from DataTables
+            tempMarker.bindPopup(feature.properties.Dir_entry1);
+
+            // Zoom to the temporary marker
+            map.setView(tempMarker.getLatLng(), 13);
+
+            // Open the popup of the temporary marker
+            tempMarker.openPopup();
         });
     });
 });
