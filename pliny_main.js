50d2aeff6079ca5a8afb76eb48aedcb925d58d74
Ryan Horne <ryan.matthew.horne@gmail.com>
2023-07-13 13:22:38 -0700

    Large updates to the codebase; included the cawm basemap

--- pliny_main.js
+++ pliny_main.js
@@ -2,11 +2,18 @@ $(document).ready(function() {
     // Initialize Leaflet map
     var map = L.map('map').setView([51.505, -0.09], 13);
 
+    var mapBounds = new L.LatLngBounds(
+        new L.LatLng(-85.051129, -180),
+        new L.LatLng(85.051129, 180));
+    var mapMinZoom = 1;
+    var mapMaxZoom = 11;
+
     // Add the base tile layer to the map
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-        maxZoom: 18,
-    }).addTo(map);
+    L.tileLayer('https://cawm.lib.uiowa.edu/tiles/{z}/{x}/{y}.png', {
+        minZoom: mapMinZoom, maxZoom: mapMaxZoom,
+        bounds: mapBounds,
+        opacity: 0.85
+      }).addTo(map);
 
     // Initialize DataTable
     var datatable = $('#datatable').DataTable();
@@ -16,61 +23,118 @@ $(document).ready(function() {
 
     // Read the GeoJSON file
     $.getJSON('Pliny_Points.geojson', function(data) {
-        // Initialize an empty array to store markers
-        var markers = [];
+        // Create a new empty SVG layer
+        var svgLayer = L.svg();
+        svgLayer.addTo(map);
 
-        // Loop through each feature
-        data.features.forEach(function(feature) {
-            // Get the coordinates of the feature
-            var coordinates = feature.geometry.coordinates;
+        // Create a D3 selection for the SVG layer
+        var svg = d3.select('#map').select('svg').select('g');
 
-            // Create a marker at the coordinates
-            var marker = L.marker([coordinates[1], coordinates[0]]);
-            
-            // Add a popup to the marker with data from DataTables
-            marker.bindPopup(feature.properties.Dir_entry1);
+        // Bind data to SVG elements
+        var features = svg.selectAll('path')
+            .data(data.features)
+            .enter()
+            .append('path')
+            .attr('d', function(d) {
+                // Customize the shape based on the Ftr_Type property
+                var ftrType = d.properties.Ftr_Type;
+                if (ftrType === 'town') {
+                    return d3.symbol().type(d3.symbolCircle)();
+                } else if (ftrType === 'mountain') {
+                    return d3.symbol().type(d3.symbolSquare)();
+                } else {
+                    return d3.symbol().type(d3.symbolTriangle)();
+                }
+            })
+            .style('fill', function(d) {
+                // Customize the fill color based on the Ftr_Type property
+                var ftrType = d.properties.Ftr_Type;
+                if (ftrType === 'town') {
+                    return 'white';
+                } else if (ftrType === 'mountain') {
+                    return 'brown';
+                } else {
+                    return 'red';
+                }
+            })
+            .attr('stroke', 'black')
+            .attr('stroke-width', 1)
+            .style("pointer-events", "auto");
 
-            // Add the marker to the map
-            marker.addTo(map);
-
-            // Store the marker in the markers array
-            markers.push(marker);
-
-            // Add a row to the DataTable
-            datatable.row.add([feature.properties.Dir_entry1]).draw();
+        // Attach feature properties to the corresponding SVG markers
+        features.each(function(d, i) {
+            var marker = d3.select(this);
+            marker.node().__data__.properties = d.properties;
         });
 
-        // Handle row click event
-        $('#datatable tbody').on('click', 'tr', function() {
+        // Handle click events on the SVG markers
+        features.on('click', function(d) {
             // Remove the previous temporary marker if exists
             if (tempMarker) {
                 map.removeLayer(tempMarker);
             }
 
+            // Get the coordinates of the clicked feature
+            var coordinates = d.currentTarget.__data__.geometry.coordinates;
+
+            // Create a temporary marker with a customized shape and style
+            tempMarker = L.marker([coordinates[1], coordinates[0]], {
+                icon: L.divIcon({
+                    className: 'custom-marker',
+                    html: '<div style="background-color: orange;"></div>',
+                    iconSize: [16, 16]
+                })
+            }).addTo(map);
+
+            // Open the popup
+            tempMarker.bindPopup(d.currentTarget.__data__.properties.Dir_entry1).openPopup();
+        });
+
+        // Update the SVG elements on map move/zoom
+        map.on('moveend', update);
+
+        // Initial update of the SVG elements
+        update();
+
+        function update() {
+            // Update the position of SVG elements based on map coordinates
+            features.attr('transform', function(d) {
+                var point = map.latLngToLayerPoint([d.geometry.coordinates[1], d.geometry.coordinates[0]]);
+                return 'translate(' + point.x + ',' + point.y + ')';
+            });
+        }
+
+        // Loop through each feature
+        data.features.forEach(function(feature) {
+            // Add a row to the DataTable
+            datatable.row.add([feature.properties.Dir_entry1]).draw();
+        });
+
+        // Zoom to a feature and open a popup when the row is clicked
+        $('#datatable tbody').on('click', 'tr', function() {
             // Get the index of the clicked row
             var index = datatable.row(this).index();
 
             // Get the corresponding feature from the GeoJSON
             var feature = data.features[index];
 
-            // Get the coordinates from DataTables
+            // Get the coordinates from the feature
             var coordinates = feature.geometry.coordinates;
 
-            // Create a temporary marker with an orange circle
-            tempMarker = L.circleMarker([coordinates[1], coordinates[0]], {
-                color: 'orange',
-                radius: 8,
-                fillOpacity: 1
+            // Create a temporary marker with a customized shape and style
+            tempMarker = L.marker([coordinates[1], coordinates[0]], {
+                icon: L.divIcon({
+                    className: 'custom-marker',
+                    html: '<div style="background-color: orange;"></div>',
+                    iconSize: [16, 16]
+                })
             }).addTo(map);
             
-            // Add a popup to the temporary marker with data from DataTables
-            tempMarker.bindPopup(datatable.row(index).data()[0]);
+            // Open the popup
+            tempMarker.bindPopup(feature.properties.Dir_entry1).openPopup();
 
-            // Zoom to the temporary marker
+            // Zoom to the feature
             map.setView(tempMarker.getLatLng(), 13);
-
-            // Open the popup of the temporary marker
-            tempMarker.openPopup();
         });
     });
 
